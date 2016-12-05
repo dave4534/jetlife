c39788285a47af32629911e57e8c6bbef39d3f40
Nathaniel Katz <Nathaniel Katz>
2016-12-05 10:32:23 +0200

    added a fly ass method

--- jet.js
+++ jet.js
@@ -3,3 +3,17 @@ var Jet = function (color, size) {
   this.size = size;
 };
 module.exports = Jet;
+
+
+//fly ass method
+var Jet = function (color, size) {
+  this.color = color;
+  this.size = size;
+
+  this.fly = function () {
+    console.log('whoosh!');
+  }
+}
+
+module.exports = Jet;
+
